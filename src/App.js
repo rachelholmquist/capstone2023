30b3834d4f1ab118b77329c83645e533621dbc1d
rachelholmquist <Rachelflorek18@outlook.com>
2023-05-22 13:06:41 -0700

    working on header css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import mainBalconyView from './images/balconyview.jpg';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header />
+      <div className="App-main">
+        <img src={mainBalconyView} className="App-image" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
-const Header = () => {
+// import {NavLink} from 'react-router-dom'
 
+const Header = () => {
+    return (
+        <nav className="App-header">
+            <ul>
+                <li><a href className='App-link'>Home</a></li>
+                <li><a href className='App-link'>Accomodations</a></li>
+                <li>Current Packages</li>
+                <li>Activities & Amenities</li>
+                <li>Your Account</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Header
