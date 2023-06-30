285ac286e0ff4b65efd19676a50a267180dde855
rachelholmquist <Rachelflorek18@outlook.com>
2023-06-29 17:37:03 -0700

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
         className="App-link"
         style={activeLinkStyle}
          to="auth">
-            Your Account          
+            Sign In          
         </NavLink>
         <Button className="Book">BOOK NOW</Button>
       </nav>
